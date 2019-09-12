a1c823088f3dae8abe3eddc5b8ca4d38e3cedc0b
Lucid Method <LucidMethod@users.noreply.github.com>
2019-09-12 14:07:58 -0500

    checkin

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("sharpAHK_Dll")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("sharpAHK")]
+[assembly: AssemblyDescription("AutoHotkey Interop/Wrapper/Expanded Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("sharpAHK_Dll")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("LucidMethod")]
+[assembly: AssemblyProduct("by LucidMethod")]
+[assembly: AssemblyCopyright("Copyright ©  2018-3000")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("95a849a0-5b6d-4d3b-a087-08647776988d")]
+[assembly: Guid("8a7cbbfe-ccb2-42ef-b90f-d937ae6ed741")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
+
+[assembly: InternalsVisibleTo("AutoHotkey.Interop.Tests")]
